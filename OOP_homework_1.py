4d34c46754c8bcd1221ce30c385752037df00f06
Rodionova Olga <oleandr865@gmail.com>
2023-11-06 19:46:16 +0300

    second commit

--- OOP_homework_1.py
+++ OOP_homework_1.py
@@ -17,23 +17,9 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+class Lecturer(Mentor):
+      pass
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
+class Reviewer(Mentor):
+      pass
