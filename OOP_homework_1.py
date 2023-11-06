075566f45fb48f18cc5f8155e3d51a03e3a1ebfc
Rodionova Olga <oleandr865@gmail.com>
2023-11-06 20:42:20 +0300

    finish commit

--- OOP_homework_1.py
+++ OOP_homework_1.py
@@ -17,7 +17,44 @@ class Student:
             else:
                 lecturer.grades_lecturer[course] = [grade]
         else:
-            return 'Ошибка'  
+            return 'Ошибка' 
+
+
+    def grades_average(self):
+        grades_count = 0
+        grades_sum = 0
+        for grade in self.grades_student:
+            grades_count += len(self.grades_student[grade])
+            grades_sum += sum(self.grades_student[grade])
+        if grades_count > 0:
+            return grades_sum / grades_count
+        else:
+            return 0
+
+
+    def __gt__(self, other):
+        if not isinstance(other, Student):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() > other.grades_average()
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() < other.grades_average()
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() == other.grades_average()
+
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {round(self.grades_average(), 0)}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {', '.join(self.finished_courses)}\n") 
 
      
 class Mentor:
@@ -26,7 +63,7 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-        
+
 class Lecturer(Mentor):
     lecturer_list = []
     def __init__(self, name, surname):
@@ -48,6 +85,29 @@ class Lecturer(Mentor):
             return 0
 
 
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() > other.grades_average()
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() < other.grades_average()
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Сравниваются объекты разных классов')
+            return
+        return self.grades_average() == other.grades_average()  
+
+    
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {round(self.grades_average_(), 0)}\n")
+
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
          super().__init__(name, surname)
@@ -61,3 +121,114 @@ class Reviewer(Mentor):
                 student.grades_student[course] = [grade]
         else:
             return 'Ошибка'
+        
+    
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n")
+    
+
+def courses_average_students(student_list, course):
+    for student in student_list:
+        for cours_name, average in student.grades_student.items():
+            if course == cours_name:
+                sum_average = sum(average) / len(average)
+                print(f"Студент: {student.name} {student.surname}\n"
+                    f"Курс: {cours_name}\n"
+                    f"Cредняя оценка за домашние задания: {round(sum_average, 0)}\n")
+
+
+def courses_average_lecturer(lecturer_list, course):
+    for lecturer in lecturer_list:
+        for cours_name, average in lecturer.grades_lecturer.items():
+            if course == cours_name:
+                sum_average = sum(average) / len(average)
+                print(f"Лектор: {lecturer.name} {lecturer.surname}\n"
+                    f"Курс: {cours_name}\n"
+                    f"Cредняя оценка за домашние задания: {round(sum_average, 0)}\n")
+
+
+student_1 = Student('Ruoy', 'Eman', 'your_gender')
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
+student_1.finished_courses += ['Введение в программирование']
+
+student_2 = Student('Kris', 'Wood', 'your_gender')
+student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
+student_2.finished_courses += ['Введение в программирование']
+
+mentor_reviewer_1 = Reviewer('Some', 'Buddy')
+mentor_reviewer_1.courses_attached += ['Python']
+mentor_reviewer_1.courses_attached += ['Git']
+
+mentor_reviewer_2 = Reviewer('Once', 'Eler')
+mentor_reviewer_2.courses_attached += ['Python']
+mentor_reviewer_2.courses_attached += ['Git']
+
+mentor_lecturer_1 = Lecturer('Yor', 'Forger')
+mentor_lecturer_1.courses_attached += ['Python']
+mentor_lecturer_1.courses_attached += ['Git']
+
+mentor_lecturer_2 = Lecturer('Lloid', 'Forger')
+mentor_lecturer_2.courses_attached += ['Python']
+mentor_lecturer_2.courses_attached += ['Git']
+
+mentor_reviewer_1.rate_hw_student(student_1, 'Python', 9)
+mentor_reviewer_1.rate_hw_student(student_1, 'Python', 10)
+mentor_reviewer_1.rate_hw_student(student_1, 'Python', 8)
+mentor_reviewer_1.rate_hw_student(student_1, 'Git', 10)
+mentor_reviewer_1.rate_hw_student(student_1, 'Git', 9)
+
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 9)
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 8)
+mentor_reviewer_1.rate_hw_student(student_2, 'Git', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Git', 8)
+
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 9)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 10)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 9)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Git', 8)
+
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 7)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 9)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Git', 8)
+
+print(f'Оценки студента {student_1.name} {student_1.surname}: ', *[f"{key}: {', '.join(map(str, value))}" for key, value in student_1.grades_student.items()])
+print(f'Оценки студента {student_2.name} {student_2.surname}: ', *[f"{key}: {', '.join(map(str, value))}" for key, value in student_2.grades_student.items()])
+print(f'Курсы студента {student_1.name} {student_1.surname}: ', ', '.join(student_1.courses_in_progress))
+print(f'Курсы студента {student_2.name} {student_2.surname}: ', ', '.join(student_2.courses_in_progress))
+
+print(f'Оценки лектора {mentor_lecturer_1.name} {mentor_lecturer_1.surname}: ', *[f"{key}: {', '.join(map(str, value))}" for key, value in mentor_lecturer_1.grades_lecturer.items()])
+print(f'Оценки лектора {mentor_lecturer_2.name} {mentor_lecturer_2.surname}: ', *[f"{key}: {', '.join(map(str, value))}" for key, value in mentor_lecturer_2.grades_lecturer.items()])
+
+print(mentor_reviewer_1)
+print(mentor_lecturer_1)
+print(mentor_lecturer_2)
+print(student_1)
+print(student_2)
+
+if student_1 > student_2:
+    print(f'Средняя оценка {student_1.name} {student_1.surname} больше, чем средняя оценка {student_2.name} {student_2.surname}')
+elif student_1 < student_2:
+    print(f'Средняя оценка {student_1.name} {student_1.surname} меньше, чем средняя оценка {student_2.name} {student_2.surname}')
+else:
+    print(f'Средняя оценка {student_1.name} {student_1.surname}  равна средней оценке {student_2.name} {student_2.surname}')
+print()
+
+
+if mentor_lecturer_1 > mentor_lecturer_1:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname} больше, чем средняя оценка {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+elif mentor_lecturer_1 < mentor_lecturer_1:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname} меньше, чем средняя оценка {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+else:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname}  равна средней оценке {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+print()
+
+
+courses_average_students(Student.student_list, 'Python')
+courses_average_lecturer(Lecturer.lecturer_list, 'Git')
+
+print('ok')
