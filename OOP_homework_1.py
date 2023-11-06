b280ff7fd1bb8774f8cea155d7fb728ff10c791a
Rodionova Olga <oleandr865@gmail.com>
2023-11-06 20:22:37 +0300

    third commit

--- OOP_homework_1.py
+++ OOP_homework_1.py
@@ -6,9 +6,18 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.grades_student = {}
+        Student.student_list.append(self)
 
-    def add_courses(self, course_name):
-   		self.finished_courses.append(course_name)   
+
+    def rate_hw_lecturer(self, lecturer, course, grade):         
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades_lecturer:
+                lecturer.grades_lecturer[course] += [grade]
+            else:
+                lecturer.grades_lecturer[course] = [grade]
+        else:
+            return 'Ошибка'  
 
      
 class Mentor:
@@ -17,9 +26,38 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+        
 class Lecturer(Mentor):
-      pass
+    lecturer_list = []
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades_lecturer = {}
+        Lecturer.lecturer_list.append(self)
+    
+
+    def grades_average_(self):
+        grades_count = 0
+        grades_sum = 0
+        for grade in self.grades_lecturer:
+            grades_count += len(self.grades_lecturer[grade])
+            grades_sum += sum(self.grades_lecturer[grade])
+        if grades_count > 0:
+            return grades_sum / grades_count
+        else:
+            return 0
 
 
 class Reviewer(Mentor):
-      pass
+    def __init__(self, name, surname):
+         super().__init__(name, surname)
+
+    
+    def rate_hw_student(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades_student:
+                student.grades_student[course] += [grade]
+            else:
+                student.grades_student[course] = [grade]
+        else:
+            return 'Ошибка'
